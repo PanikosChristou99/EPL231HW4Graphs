0daac0a444fa5eb28c702108f4d84e077e532ec4
Stylianos Herodotou <49488837+sherod01@users.noreply.github.com>
2019-11-27 13:02:44 +0000

    Update edge.java

--- src/edge.java
+++ src/edge.java
@@ -1,49 +1,113 @@
-/**an object htat has a pointer to 2 nodes and an int that represent as its name and edge of a graph
- * we ude it in the mst to help with kruskal
- * 
- * @author panikos
- *
- */
-public class edge {
-private node n1;
-private node n2;
-private double weight;
 
-public edge(node n1, node n2,double weight) {
-	this.n1 = n1;
-	this.n2 = n2;
-	this.weight = weight;
+
+import java.util.ArrayList;
+
+public class node {
+private int x;
+private int y;
+private int id;
+private double temp;
+private ArrayList<neighbour> neighbours;
+int thesiStoPinaka;
+private node next;
+
+ public node getNext() {
+	return next;
+}
+public void setNext(node next) {
+	this.next = next;
+}
+public int getThesiStoPinaka() {
+	return thesiStoPinaka;
+}
+public void setThesiStoPinaka(int thesiStoPinaka) {
+	this.thesiStoPinaka = thesiStoPinaka;
 }
 
-public static edge copyEdge(edge original)
+public node(int x, int y, int id,double temp) {
+	 this.x =x;
+	 this.y =y;
+	 this.id =id;
+	 this.temp =temp;
+	 this.neighbours = new ArrayList<neighbour>();
+	 this.next=null;
+}
+public void addNeigb(neighbour n) {
+	this.neighbours.add(n);
+}
+public void removeNeigh(neighbour n) {
+	for (int i = 0; i < neighbours.size(); i++) {
+		if(n.equals(neighbours.get(i))) {
+			neighbours.remove(i);
+			break;
+		}
+	}
+}
+
+//
+//public ArrayList<edge> getEdges() {
+//	ArrayList<edge> a = new ArrayList<edge>();
+//	for (int i = 0; i < this.neighbours.size(); i++) {
+//		a.add(new edge(this, neighbours.get(i),weights.get(i)));
+//	}
+//	return a;
+//}
+
+public double isNeighborPlusD(node n2, double d)
 {
-	edge temp=new edge(original.n1,original.n2, original.weight);
+	double distance=findDistance(this, n2);
+	if(distance<=d)
+	{
+		return distance;
+	}
+	return -1;
+}
+
+public static double findDistance(node n1, node n2)
+{
+	double alfa= (n1.x-n2.getX())* (n1.x-n2.getX());
+	double vita= (n1.y-n2.getY())*(n1.y-n2.getY());
+	double distance=Math.sqrt(alfa+vita);
+	return distance;
+}
+
+public int getX() {
+	return x;
+}
+public void setX(int x) {
+	this.x = x;
+}
+public int getY() {
+	return y;
+}
+public void setY(int y) {
+	this.y = y;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public double getTemp() {
 	return temp;
 }
+public void setTemp(double temp) {
+	this.temp = temp;
+}
 
-public int compareTo(edge kati)
+public void removeNeighbour(node n)
 {
-	if(this.weight>kati.weight)
+	for(int i=0; i<this.neighbours.size(); i++)
 	{
-		return 1;
-	}
-	else if(this.weight<kati.weight)
-	{
-		return -1;
-	}
-	else 
-	{
-		return 0;
+		if(this.neighbours.get(i).getN1().equals(n))
+		{
+			this.neighbours.remove(i);
+			break;
+		}
 	}
 }
-public boolean isOneOfNodes(node n)
-{
-	if((this.getN1().equals(n))||(this.getN2().equals(n)))
-	{
-		return true;
-	}
-	return false;
-}
+
 
 @Override
 public boolean equals(Object obj) {
@@ -53,46 +117,33 @@ public boolean equals(Object obj) {
 		return false;
 	if (getClass() != obj.getClass())
 		return false;
-	edge other = (edge) obj;
-	if (n1 == null) {
-		if (other.n1 != null)
-			return false;
-	} else if (!n1.equals(other.n1))
+	node other = (node) obj;
+	if (id != other.id)
 		return false;
-	if (n2 == null) {
-		if (other.n2 != null)
+	if (neighbours == null) {
+		if (other.neighbours != null)
 			return false;
-	} else if (!n2.equals(other.n2))
+	} else if (!neighbours.equals(other.neighbours))
 		return false;
-	if (weight != other.weight)
+	if (Double.doubleToLongBits(temp) != Double.doubleToLongBits(other.temp))
+		return false;
+	if (x != other.x)
+		return false;
+	if (y != other.y)
 		return false;
 	return true;
 }
 
+public ArrayList<neighbour> getNeighbours() {
+	return neighbours;
+}
+public void setNeighbours(ArrayList<neighbour> neighbours) {
+	this.neighbours = neighbours;
+}
 @Override
 public String toString() {
-	return "edge [n1=" + n1 + ", n2=" + n2 + ", weight=" + weight + "]";
+	return "node [x=" + x + ", y=" + y + ", id=" + id + ", temp=" + temp + ", neighbours=" + neighbours
+			+ ", thesiStoPinaka=" + thesiStoPinaka + "]";
 }
-
-public node getN1() {
-	return n1;
-}
-public void setN1(node n1) {
-	this.n1 = n1;
-}
-public node getN2() {
-	return n2;
-}
-public void setN2(node n2) {
-	this.n2 = n2;
-}
-public double getWeight() {
-	return weight;
-}
-public void setWeight(int weight) {
-	this.weight = weight;
-}
-
-
 
 }
