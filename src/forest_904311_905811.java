49248e0a801b3c7c8d2e469d2c6c0c5dd304ecc4
Panikos Christou <panikoschristou@yahoo.com>
2019-11-27 10:24:50 +0200

    Update forest_904311_905811.java

--- src/forest_904311_905811.java
+++ src/forest_904311_905811.java
@@ -19,15 +19,15 @@ import java.io.IOException;
 public class forest_904311_905811 {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length > 1 || args.length == 0) {
-			System.out.println("en evales i evales pano apo 2 arguments bye");
+		if (args.length !=2) {
+			System.out.println("en evales 2 arguments bye");
 			System.exit(0);
 
 		}
-		double d = 10;
+		double d = Integer.valueOf(args[0]);
 		Graph g = new Graph(d);
 		try {
-			File file = new File(args[0]);
+			File file = new File(args[1]);
 			Scanner sc = new Scanner(file);
 //			long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 //			long startTime = System.currentTimeMillis();
@@ -35,11 +35,13 @@ public class forest_904311_905811 {
 			while (sc.hasNextLine()) {// insert the dictionary to the trie
 				String str = sc.nextLine();
 				int x = Integer.parseInt(str.substring(str.indexOf("(") + 1, str.indexOf(",")));
-				int y = Integer.parseInt(str.substring(str.indexOf(",") + 1, str.indexOf(")")));
-				int id = Integer.parseInt(str.substring(0, str.indexOf("(")));
-				int temp = Integer.parseInt(str.substring(str.indexOf(")") + 1, str.length() - 1));
-				node n = new node(x, y, id, temp);
-				g.addNode(n);
+				int y = Integer.parseInt(str.substring(str.indexOf(",") + 2, str.indexOf(")")));
+				int id = Integer.parseInt(str.substring(0, str.indexOf("(")-1));
+				int temp = Integer.parseInt(str.substring(str.indexOf(")") + 2, str.length()));
+				System.out.println(x + y+  id +temp );
+				g.addNodeAdvance(x, y, id, temp);
+				
+				
 			}
 
 //			long endTime = System.currentTimeMillis();
@@ -54,7 +56,8 @@ public class forest_904311_905811 {
 			System.out.println("i couldnt find the file");
 			System.exit(0);
 		}
-
+System.out.println(g.toString());
+g.getMst().createMinimumSpanningTree(g);
 		int option = 1;
 		Scanner in = new Scanner(System.in);
 
@@ -89,7 +92,8 @@ public class forest_904311_905811 {
 					int y = in.nextInt();
 					System.out.println("dose m temp :");
 					double t = in.nextDouble();
-					g.addNode(x, y, id, t);
+					g.addNodeAdvance(x, y, id, t);
+					System.out.println(g.toString());
 					System.out.println();
 					continue;
 
@@ -97,6 +101,7 @@ public class forest_904311_905811 {
 					System.out.println("dose m id :");
 					id = in.nextInt();
 					g.removeNode(id);
+					System.out.println(g.toString());
 					System.out.println();
 				case 5:
 					System.out.println("dose m id :");
@@ -104,6 +109,7 @@ public class forest_904311_905811 {
 					System.out.println("dose m temp :");
 					double temp = in.nextDouble();
 					g.search(id).setTemp(temp);
+					System.out.println(g.toString());
 					System.out.println();
 					continue;
 				case 6:
@@ -111,7 +117,7 @@ public class forest_904311_905811 {
 					int id1 = in.nextInt();
 					System.out.println("dose m id2 :");
 					int id2 = in.nextInt();
-					transferFromAtoB(id1, id2, g);
+					removeEdgesAndPrintPath(transferFromAtoB(id1, id2, g),id1);
 					System.out.println();
 					continue;
 				case 7:
@@ -134,9 +140,8 @@ public class forest_904311_905811 {
 
 		}
 
-		File file = new File(args[0]);
+		File file = new File(args[1]);
 		 BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
 		for (int i = 0; i < g.getHashtable().length; i++) {
 			LinkedList<node> list = g.getHashtable(i);
 			if (list.isEmpty() == true) {
@@ -152,6 +157,29 @@ public class forest_904311_905811 {
 				}
 			}
 		}
+	}
+	public static void removeEdgesAndPrintPath(ArrayDeque<edge> arE,int idA) {
+		
+		ArrayDeque<Integer> ar = new  ArrayDeque<Integer>();
+		edge e = arE.pop();
+		node other = returnOtherNode(e,idA);
+		node want = returnOtherNode(e,idA);
+		double  temp = want.getTemp();
+		ar.push(other.getId());
+		while(!arE.isEmpty()) {
+			 e = arE.pop();
+			 other = returnOtherNode(e,idA);
+			 ar.push(other.getId());
+		}
+		System.out.println("the path is : \n");
+		while (!ar.isEmpty()) {
+			System.out.println(ar.pop().toString()+"\n");
+		}
+		System.out.println("and the temp of what you wanted is : " + temp);
+	}
+	public static node returnOtherNode(edge e,int idA) {
+	if(e.getN1().getId()==idA) return e.getN2();
+	else return e.getN1();
 	}
 	public static ArrayDeque<edge> transferFromAtoB(int idA, int idB, Graph g) {
 		if (idA == idB) {
@@ -169,16 +197,27 @@ public class forest_904311_905811 {
 		while (!q.isEmpty()) {
 			temp = q.remove();
 			if (temp.peek().getN1().getId() == idB || temp.peek().getN2().getId() == idB) {
+				
+				for (int i = 0; i < g.getMst().getMedges().size();i++) {
+					g.getMst().getTaken().set(i, false);
+				}
 				return temp;
+				
 			}
 		e = searchThroughEdgesForMatch(temp.peek().getN1().getId(), g);
+		
 		ArrayList<edge>e2 = searchThroughEdgesForMatch(temp.peek().getN2().getId(), g);
-			e.addAll(e2);
+			
+		e.addAll(e2);
+		
 			for (int i = 0; i < e.size(); i++) {
 				ArrayDeque<edge> temp2 = temp.clone();
 				temp2.add(e.get(i));
 				q.add(temp2);
 			}
+		}
+		for (int i = 0; i < g.getMst().getMedges().size();i++) {
+			g.getMst().getTaken().set(i, false);
 		}
 		return null;
 	}
@@ -198,6 +237,8 @@ public class forest_904311_905811 {
 			}
 		}
 		return e;
+		
 	}
 
 }
+
